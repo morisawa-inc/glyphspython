db27b81bd09416aad10918bc83a673ab9c5fbc26
Takaaki Fuji <tfuji@morisawa.co.jp>
2017-05-23 13:36:49 +0900

    Fix issue that UFO cannot be imported
    Instead of the original GSMenu implementation, an alternative app
    delegate will be set up to hold the reference to each plugin bundle.
    Note that - [NSWindowController showWindow:] are also swizzled to
    suppress the exception which happens when creating/showing a new window.

--- glyphspython/glyphspython.m
+++ glyphspython/glyphspython.m
@@ -23,6 +23,58 @@
 #import <Python/Python.h>
 
 #import <AppKit/AppKit.h>
+#import <objc/runtime.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@property (nonatomic, readonly) NSArray<NSBundle *> *filterBundles;
+@property (nonatomic, readonly) NSArray<NSBundle *> *fileFormatBundles;
+@property (nonatomic, readonly) NSArray<NSBundle *> *paletteBundles;
+@property (nonatomic, readonly) NSArray<NSBundle *> *customPluginBundles;
+@end
+
+@implementation AppDelegate
+
+- (instancetype)init {
+    if ((self = [super init])) {
+        NSMutableArray *mutableFilterBundles = [[NSMutableArray alloc] initWithCapacity:0];
+        NSMutableArray *mutableFileFormatBundles = [[NSMutableArray alloc] initWithCapacity:0];
+        for (NSString *filename in [[NSFileManager defaultManager] contentsOfDirectoryAtPath:@"/Applications/Glyphs.app/Contents/Plugins" error:nil]) {
+            NSString *extension = [filename pathExtension];
+            if ([extension isEqualToString:@"glyphsFilter"]) {
+                NSBundle *bundle = [NSBundle bundleWithPath:[@"/Applications/Glyphs.app/Contents/Plugins" stringByAppendingPathComponent:filename]];
+                if (bundle) {
+                    [bundle load];
+                    [mutableFilterBundles addObject:bundle];
+                }
+            } else if ([extension isEqualToString:@"glyphsFileFormat"]) {
+                NSBundle *bundle = [NSBundle bundleWithPath:[@"/Applications/Glyphs.app/Contents/Plugins" stringByAppendingPathComponent:filename]];
+                if (bundle) {
+                    [bundle load];
+                    [mutableFileFormatBundles addObject:bundle];
+                }
+            }
+        }
+        _filterBundles = [mutableFilterBundles copy];
+        _fileFormatBundles = [mutableFileFormatBundles copy];
+    }
+    return self;
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation {
+    if ([self respondsToSelector:[invocation selector]]) {
+        [super forwardInvocation:invocation];
+    }
+}
+    
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    NSMethodSignature *signature = [super methodSignatureForSelector:selector];
+    if (!signature) {
+        signature = [NSMethodSignature signatureWithObjCTypes:"v@:"];
+    }
+    return signature;
+}
+
+@end
 
 @interface NSApplication (GSApplicationAdditions)
 + (NSApplication *)sharedApplication;
@@ -34,10 +86,41 @@
     // A dirty hack to replace the global 'Glyphs' object with GSApplication instead of NSApplication.
     static dispatch_once_t once;
     static NSApplication *sharedInstance = nil;
-    dispatch_once(&once, ^{ sharedInstance = [[NSClassFromString(@"GSApplication") alloc] init]; });
+    static AppDelegate *delegate = nil;
+    dispatch_once(&once, ^{
+        delegate = [[AppDelegate alloc] init];
+        sharedInstance = [[NSClassFromString(@"GSApplication") alloc] init];
+        [sharedInstance setDelegate:delegate];
+    });
     return sharedInstance;
 }
 
+@end
+
+@implementation NSWindowController (NSWindowControllerAdditions)
+    
++ (void)load {
+    // Swizzle - showWindow: with the empty implementation to maintain its headless behaviour.
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+        Class class = [self class];
+        SEL originalSelector = @selector(showWindow:);
+        SEL swizzledSelector = @selector(_showWindow:);
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+    
+- (void)_showWindow:(id)sender {
+    return;
+}
+    
 @end
 
 int main(int argc, const char * argv[]) {
